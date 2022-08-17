424b01b251c1dfa7977cccd7780ae873a21b0873
Victor Rodas <victormatiasrodas100@gmail.com>
2022-08-17 01:29:16 -0400

    implemented text colors in terminal and fixed some bugs

--- setup.py
+++ setup.py
@@ -1,9 +1,44 @@
 # from wspBot import bot
-#import json
+import json
 import inquirer
 import os, platform
 
 PATH_OF_EXE = f"{os.getcwd()}\\bot.exe"
+
+COLORS = {
+    'black': '30', 'red': '31', 'green': '32',
+    'yellow': '33', 'blue': '34', 'magenta': '35',
+    'white': '37'
+}
+
+def cprint(color:str, text:str, reset:bool=True) -> None:
+
+    for key_color, value in COLORS.items():
+        if key_color == color.lower():
+            print(f"\033[1;{value}m{text}")
+            if reset: print('\033[1;0m', end='')
+            return
+    
+    print(text)
+
+def cinput(color:str, text:str) -> str:
+
+    for key_color, value in COLORS.items():
+        if key_color == color.lower():
+            return_value = input(f"\033[1;{value}m{text}")
+            print('\033[1;0m', end='')
+            return return_value
+    
+    raise Exception("ERROR: Wrong color was given")
+
+def write_json(data:dict) -> None:
+    """
+    This function setup the config JSON
+    """
+    JSON_object = json.dumps(data, indent=4)
+
+    with open('config.json', 'w') as f:
+        f.write(JSON_object)
 
 def make_question(message:str, choices:list) -> str:
     questions = [
@@ -129,12 +164,12 @@ def get_hours() -> list:
     
     return hours
 
-def setup_frequency():
+def setup_frequency() -> tuple:
     data = {}
     options = ["Diariamente", "Semanalmente", "Cancelar configuración y salir"]
 
     clear_display()
-    print("Ingrese las configuraciones iniciales para utilizar el programa\n")
+    cprint("Blue", "Ingrese las configuraciones iniciales para utilizar el programa\n")
     frequency = make_question("Escoja la frecuencia de ejecución", options)
 
     if frequency == "Diariamente":
@@ -142,7 +177,7 @@ def setup_frequency():
         command = get_command('Daily', [hours])
 
         data['frequency'] = 'Daily'
-        data['time'] = hours
+        data['times'] = hours
 
     elif frequency == "Semanalmente":
         day = make_question(
@@ -156,15 +191,31 @@ def setup_frequency():
         command = get_command('Weekly', [day_english, hours])
 
         data['frequency'] = 'Weekly'
-        data['time'] = {day_english: hours}
+        data['times'] = {day_english: hours}
 
     elif frequency[0] == 'C':
         exit()
     else:
         setup_frequency()
 
-    data['tasks'] = len(hours) #Amount of tasks
+    data['amount_tasks'] = len(hours) #Amount of tasks
+
+    write_json(data)
     return command
+
+def get_error_message_for_path(message:str, aux_message:str) -> tuple:
+    """
+    This function is for verify_path()
+    """
+
+    status = False
+
+    if not message:
+        message = aux_message
+    else:
+        message = f"{message} y {aux_message.lower()}"
+    
+    return status, message
 
 def verify_path(path:str) -> tuple:
     if path.lower().strip() == 'x': return True, ''
@@ -176,14 +227,15 @@ def verify_path(path:str) -> tuple:
     if '\\' in path:
         status = False
         message = "No ingrese backslashes ['\\']"
-    if path[-4:] != '.txt':
-        status = False
-        aux_message = "Debe ingresar el archivo .txt al final de la ruta"
+    
+    if '/' not in path:
+        status, message = get_error_message_for_path(message, "Ingrese las rutas separadas por slashes ['/']")
+    
+    if '//' in path:
+        status, message = get_error_message_for_path(message, "Formato no adecuado para las rutas")
 
-        if not message:
-            message = aux_message
-        else:
-            message = f"{message} y {aux_message.lower()}"
+    if path[-4:] != '.txt':
+        status, message = get_error_message_for_path(message, "Debe ingresar el archivo .txt al final de la ruta")
 
     return status, message
 
@@ -214,64 +266,75 @@ def show_paths(paths:list, err:str='') -> list:
     for _ in answers:
         for answer in _:
             exists_answer = True
-            paths.remove(answer) #This doesn't remove the path in the list
+            paths.remove(answer)
 
     if not exists_answer:
-        return(show_paths(paths, err="ERROR: no ha seleccionado [Aprete espacio]"))
+        return(show_paths(paths, err="ERROR: No ha seleccionado [Aprete espacio]"))
     else:
-        print(f"Resultado: {paths}")
         return paths
 
-def setup_paths() -> None:
+def setup_paths() -> list:
     clear_display()
-    print("Para los mensajes se necesita un archivo txt por cada mensaje que se quiera enviar")
+    cprint("Blue", "Para los mensajes se necesita un archivo txt por cada mensaje que se quiera enviar")
     print("""
     Si los mensajes quieren ser acompañados por una imagen
-    debe ingresar la ruta absoluta de la imagen dentro del archivo de texto
-    Al inicio (Y SOLO AL INICIO) del archivo de texto se debe incluir lo siguiente:\n
-    $PATH='C:/la/ruta/aqui'
+    debe ingresar la ruta absoluta de la imagen dentro del archivo de texto.\n
+    Al inicio \033[1;31m(Y SOLO AL INICIO)\033[1;0m del archivo de texto se debe incluir lo siguiente:
     """)
+    cprint("Yellow", "    $PATH='C:/la/ruta/aqui'\n")
 
-    print("Ingrese las rutas absolutas de los mensajes [C:/Users/YourUser/Desktop/resources/msg.txt]")
-    print("- Escriba 'x' para terminar el listado")
-    print("- Escriba 'd' si desea eliminar una ruta mal ingresada")
+    print("Ingrese las rutas absolutas de los mensajes ", end='')
+    cprint("Yellow", "[C:/Users/YourUser/Desktop/resources/msg.txt]")
+
+    print("- Escriba \033[1;34m'x'\033[1;0m para terminar el listado")
+    print("- Escriba \033[1;34m'd'\033[1;0m si desea eliminar una ruta mal ingresada")
     print("- No ingrese las rutas entre comillas")
-    print("- NO INGRESE LAS RUTAS CON BACKSLASHES '\\' INGRESE LAS RUTAS CON SLASHES '/'")
+    cprint("Red", "- NO INGRESE LAS RUTAS CON BACKSLASHES '\\' INGRESE LAS RUTAS CON SLASHES \033[1;32m'/'\033[1;0m")
     print("- Si ingresa una ruta que no existe, el mensaje simplemente no será enviado")
 
     paths = []
     while True:
-        single_path = input("> ")
+        single_path = cinput("Blue", "> ")
         status_path, err_message = verify_path(single_path)
 
         while not status_path:
-            print(err_message)
-            single_path = input("> ")
+            cprint("Red", err_message)
+            single_path = cinput("Blue", "> ")
             status_path, err_message = verify_path(single_path)
         
         if single_path.lower().strip() == 'x': break
         elif single_path.lower().strip() == 'd' and len(paths) > 0:
             paths = show_paths(paths)
+            cprint("Blue", "Siga ingresando nuevas rutas o ingrese 'x' para salir")
             continue
-
-        paths.append(single_path)
+        
+        if single_path != 'd': #If the user put first of all a 'd'
+            paths.append(single_path)
     
     if len(paths) == 0:
-        response = make_question("No se ingresó ninguna ruta,¿Desea cancelar la configuración?", choices=['s', 'n'])
+        response = make_question("No se ingresó ninguna ruta,¿Desea cancelar la configuración?", choices=['Sí', 'No'])
 
-        if response == 's': exit()
+        if response[0] == 'S': exit()
 
-        elif response == 'n':
-            confirm = make_question("\n¿Ingresó alguna ruta equivocada que quiera borrar?", choices=['s', 'n'])
+        elif response[0] == 'N':
+            confirm = make_question("\n¿Ingresó alguna ruta equivocada que quiera borrar?", choices=['Sí', 'No'])
 
-            if confirm == 's': paths = show_paths(paths)
+            if confirm[0] == 'S': paths = show_paths(paths)
             else: setup_paths()
 
         else:
             setup_paths()
     
-    print(paths)
+    write_json({"messages": paths})
+    return paths
         
+def setup_links() -> None:
+    clear_display()
+    print("Ingrese los links de los grupos a los cuales mandar los mensajes")
+    print("Ingrese los links completos [https://chat.whatsapp.com/AbCdF123]")
+    print("- No ingrese los links entre comillas")
+    print("- Escriba 'x' para terminar el listado")
+    print("- Escriba 'd' para eliminar algún link mal ingresado")
 
 #setup_frequency()
 #setup_paths()
@@ -292,12 +355,13 @@ def setup_paths() -> None:
 #     except FileNotFoundError:
 #         return False
 
+# def main_setup() -> None:
 
-# if check_JSON():
-#     group_links, messages, imgs = get_data_from_config()
+#     if check_JSON():
+#         # group_links, messages, imgs = get_data_from_config()
 
-#     for link in group_links:
-#         bot(link, messages, imgs)
+#         # for link in group_links:
+#         #     bot(link, messages, imgs)
 
 # else:
 #     setup()
