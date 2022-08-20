9c074c4e60118781cde353092a0e165809578d6f
Victor Rodas <victormatiasrodas100@gmail.com>
2022-08-20 14:39:47 -0400

    added more functionality

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-import json
+from time import sleep
+import os, subprocess
 import inquirer
-import os
+import json
 
 PATH_OF_EXE = f"{os.getcwd()}\\bot.exe"
 
@@ -10,6 +11,41 @@ COLORS = {
     'white': '37'
 }
 
+
+#Powershell
+def exec_powershell() -> bool:
+    """
+    Executes the task register,
+    returns ```True``` if the script ends successfully, otherwise returns ```False```
+    """
+
+    status = True
+
+    arg = "Set-ExecutionPolicy RemoteSigned -Scope Process -Force; if ($?) { .\\setTasks.ps1 }"
+    process = subprocess.Popen(["powershell.exe", arg], stderr=subprocess.PIPE)
+    stderror = process.stderr.read()
+
+    if stderror: status = False
+
+    return status
+
+def write_script(commands:list) -> bool:
+    """
+    Creates the task register script
+    """
+
+    for command in commands:
+        with open("setTasks.ps1", 'w') as script:
+            for section in command:
+                script.write(f"{section}\n")
+
+        success_status = exec_powershell() 
+        if not success_status: break
+    
+    return success_status
+
+
+#Input-output (General)
 def cprint(color:str, text:str, reset:bool=True) -> None:
 
     for key_color, value in COLORS.items():
@@ -30,15 +66,6 @@ def cinput(color:str, text:str) -> str:
     
     raise Exception("ERROR: Wrong color was given")
 
-def write_json(data:dict) -> None:
-    """
-    This function setup the config JSON
-    """
-    JSON_object = json.dumps(data, indent=4)
-
-    with open('config.json', 'w') as f:
-        f.write(JSON_object)
-
 def make_question(message:str, choices:list) -> str:
     questions = [
         inquirer.List(
@@ -51,22 +78,69 @@ def make_question(message:str, choices:list) -> str:
     answers = inquirer.prompt(questions)
     return answers['answer']
 
-def clear_display(): os.system("cls")
+def clear_display(msg=True) -> None:
+    os.system("cls")
+    if msg: cprint("Blue", "Ingrese las configuraciones iniciales para utilizar el programa\n")
 
-def get_day_english(day:str) -> str:
+
+#Config file
+def check_config() -> bool:
+    exists = True
+    files = os.listdir()
+
+    if 'config.json' not in files:
+        exists = False
+
+    return exists
+
+def write_json(data:dict) -> None:
     """
-    Return the day in English
+    This function setup the config JSON
+    """
+    JSON_object = json.dumps(data, indent=4)
+
+    with open('config.json', 'w') as f:
+        f.write(JSON_object)
+
+def modify_json() -> None:
+    with open("config.json", 'r') as config:
+        data:dict = json.load(config)
+
+    key = make_question("¿Qué datos desea modificar?", choices=list(data.keys()))
+    entry = data[key]
+
+    if type(entry) == list:
+        if key == "group_links":
+            new_data = show_links_or_paths(entry, mode='links')
+        elif key == "messages":
+            new_data = show_links_or_paths(entry, mode='paths')
+        elif key == "times":
+            new_data = show_links_or_paths(entry, mode="horas")
+    
+        data[key] = new_data
+        write_json(data)
+
+    elif type(entry) == dict:
+        pass #TODO: modify times for weekly mode
+
+    else:
+        cprint("Red", "No implementado aún")
+
+
+#Checkers
+def get_error_message_for_path(message:str, aux_message:str) -> tuple:
+    """
+    This function is for ```verify_path()```
     """
 
-    days = {
-        "Lunes": "Monday", "Martes": "Tuesday",
-        "Miércoles": "Wednesday", "Jueves": "Thursday",
-        "Viernes": "Friday", "Sábado": "Saturday", "Domingo": "Sunday"
-    }
+    status = False
 
-    for keys, values in days.items():
-        if keys == day:
-            return values
+    if not message:
+        message = aux_message
+    else:
+        message = f"{message} y {aux_message.lower()}"
+    
+    return status, message
 
 def verify_hours(hours) -> bool:
     status = True
@@ -97,79 +171,6 @@ def verify_hours(hours) -> bool:
             status = False
     
     return not status
-
-def get_command(frequency:str, times:list) -> str:
-    commands = []
-    task = []
-    counter = 0
-
-
-    if frequency == 'Daily':
-        while counter < len(times[0]):
-            hour = times[0][counter]
-
-            action = f'$action = New-ScheduledTaskAction -Execute "{PATH_OF_EXE}";'
-            trigger = f'$trigger = New-ScheduledTaskTrigger -Daily -At {hour};'
-            register = f"Register-ScheduledTask wspbotDaily_{counter} -Action $action -Trigger $trigger;"
-
-            task.append(action)
-            task.append(trigger)
-            task.append(register)
-            commands.append(task)
-            task = []
-
-            counter+=1
-
-    elif frequency == 'Weekly':
-        while counter < len(times[1]):
-            hour = times[1][counter]
-
-            action = f'$action = New-ScheduledTaskAction -Execute "{PATH_OF_EXE}";'
-            trigger = f"$trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek {times[0]} -At {hour};"
-            register = f"Register-ScheduledTask wspbotWeek_{counter} -Action $action -Trigger $trigger;"
-
-            task.append(action)
-            task.append(trigger)
-            task.append(register)
-            commands.append(task)
-            task = []
-
-            counter+=1
-    
-    return commands
-
-def get_hours() -> list:
-    print("\nIngrese la hora de ejecución en formato de 12 horas `h:m[am/pm]`")
-    print("Si ingresa más de una hora, ingresalos separados por espacios `h1:m1am h2:m2pm`")
-
-    hours = input("> ").split(' ')
-
-    for x in range(len(hours)): #We remove the whitespacess
-        hour = hours[x]
-        hours[x] = hour.strip()
-
-    status = verify_hours(hours)
-
-    while status:
-        print("\nIngrese las horas en formato adecuado! [horas:minutos][am/pm]")
-        hours = input("> ").split(' ')
-        status = verify_hours(hours)
-    
-    return hours
-
-def get_error_message_for_path(message:str, aux_message:str) -> tuple:
-    """
-    This function is for verify_path() and verify_link()
-    """
-
-    status = False
-
-    if not message:
-        message = aux_message
-    else:
-        message = f"{message} y {aux_message.lower()}"
-    
-    return status, message
 
 def verify_path(path:str) -> tuple:
     if path.lower().strip() == 'x': return True, ''
@@ -216,6 +217,84 @@ def verify_link(link:str) -> tuple:
 
     return status, message
 
+
+#Get formatted data
+def get_day_english(day:str) -> str:
+    """
+    Return the day in English
+    """
+
+    days = {
+        "Lunes": "Monday", "Martes": "Tuesday",
+        "Miércoles": "Wednesday", "Jueves": "Thursday",
+        "Viernes": "Friday", "Sábado": "Saturday", "Domingo": "Sunday"
+    }
+
+    for keys, values in days.items():
+        if keys == day:
+            return values
+
+def get_command(frequency:str, times:list) -> str:
+    commands = []
+    task = []
+    counter = 0
+
+
+    if frequency == 'Daily':
+        while counter < len(times[0]):
+            hour = times[0][counter]
+
+            action = f'$action = New-ScheduledTaskAction -Execute "{PATH_OF_EXE}";'
+            trigger = f'$trigger = New-ScheduledTaskTrigger -Daily -At {hour};'
+            register = f"Register-ScheduledTask wspbotDaily_{counter} -Action $action -Trigger $trigger;"
+
+            task.append(action)
+            task.append(trigger)
+            task.append(register)
+            commands.append(task)
+            task = []
+
+            counter+=1
+
+    elif frequency == 'Weekly':
+        while counter < len(times[1]):
+            hour = times[1][counter]
+
+            action = f'$action = New-ScheduledTaskAction -Execute "{PATH_OF_EXE}";'
+            trigger = f"$trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek {times[0]} -At {hour};"
+            register = f"Register-ScheduledTask wspbotWeek_{counter} -Action $action -Trigger $trigger;"
+
+            task.append(action)
+            task.append(trigger)
+            task.append(register)
+            commands.append(task)
+            task = []
+
+            counter+=1
+    
+    return commands
+
+def get_hours() -> list:
+    cprint("Yellow", "\nIngrese la hora de ejecución en formato de 12 horas `h:m[am/pm]`")
+    print("Si ingresa más de una hora, ingresalos separados por espacios \033[1;34m`h1:m1am h2:m2pm`\033[1;0m")
+
+    hours = input("> ").split(' ')
+
+    for x in range(len(hours)): #We remove the whitespacess
+        hour = hours[x]
+        hours[x] = hour.strip()
+
+    status = verify_hours(hours)
+
+    while status:
+        print("\nIngrese las horas en formato adecuado! [horas:minutos][am/pm]")
+        hours = input("> ").split(' ')
+        status = verify_hours(hours)
+    
+    return hours
+
+
+#Get data from user
 def show_links_or_paths(data:list, mode:str, err:str='',) -> list:
     print("")
 
@@ -276,41 +355,6 @@ def get_link_or_path_from_user(message_delete:str, mode:str) -> list:
     
     return data
 
-def exec_powershell() -> bool:
-    """
-    Returns True if the script ends successfully, otherwise returns False
-    """
-
-    status = True
-
-    with open("./setTasks.ps1", 'r') as script:
-        text = script.read()
-    
-    print("\nCurrent script: ")
-    print(text)
-
-    return status
-
-def write_script(commands:list) -> bool:
-    status = True
-    for command in commands:
-        with open("setTasks.ps1", 'w') as script:
-            for section in command:
-                script.write(f"{section}\n")
-
-        control = exec_powershell() 
-        if not control: status = False #if the script doesn't end successfully returns False
-
-    return status
-
-def check_config() -> bool:
-    exists = True
-    files = os.listdir()
-
-    if 'config.json' not in files:
-        exists = False
-
-    return exists
 
 #Main functions
 def setup_frequency() -> tuple:
@@ -318,7 +362,6 @@ def setup_frequency() -> tuple:
     options = ["Diariamente", "Semanalmente", "Cancelar configuración y salir"]
 
     clear_display()
-    cprint("Blue", "Ingrese las configuraciones iniciales para utilizar el programa\n")
     frequency = make_question("Escoja la frecuencia de ejecución", options)
 
     if frequency == "Diariamente":
@@ -349,7 +392,9 @@ def setup_frequency() -> tuple:
 
     data['amount_tasks'] = len(hours) #Amount of tasks
 
-    write_json(data)
+    cprint("Blue", "\nTiempo configurado!\n")
+    sleep(1)
+    #write_json(data)
     return data, command
 
 def setup_paths() -> list:
@@ -388,12 +433,14 @@ def setup_paths() -> list:
         else:
             return(setup_paths())
     
-    write_json({"messages": paths})
+    cprint("Blue", "\nRutas configuradas!\n")
+    sleep(1)
+    #write_json({"messages": paths})
     return paths
         
 def setup_links() -> list:
     clear_display()
-    cprint("Blue", "Ingrese los links de los grupos a los cuales mandar los mensajes")
+    cprint("Blue", "Ingrese los links de los grupos a los cuales mandar los mensajes\n")
     print("Ingrese los links completos \033[1;33m[https://chat.whatsapp.com/AbCdF123]\033[1;0m")
     print("- No ingrese los links entre comillas")
     print("- Escriba 'x' para terminar el listado")
@@ -416,7 +463,9 @@ def setup_links() -> list:
         else:
             return(setup_links())
     
-    write_json({"links": links})
+    cprint("Blue", "\nLinks configurados!\n")
+    sleep(1)
+    #write_json({"links": links})
     return links
 
 def main_setup() -> None:
@@ -431,17 +480,22 @@ def main_setup() -> None:
     data['group_links'] = group_links
 
     write_json(data)
-    write_script(commands)
+    success_status = write_script(commands)
+
+    if not success_status: cprint("Red", "\nAn error has ocurred in the task register\n")
 
 #TODO: check for nonexistent paths
+#Unregister-ScheduledTask -TaskName "TestTask" -Confirm:$false
 
 config_exists = check_config()
+
 if config_exists:
+    clear_display(msg=False)
+    cprint("Blue", "Ya existe una configuración")
     response = make_question("¿Desea modificar las configuraciones dadas?", choices=['Sí', 'No'])
 
     if response[0] == 'S':
-        pass
-        #TODO: modify config.json
+        modify_json() #TODO: change the task schedule if times have been changed
     else:
         main_setup()
 else:
